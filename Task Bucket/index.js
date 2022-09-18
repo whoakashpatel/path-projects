7273b85d8399e0c23f7ea3df0a6494d85beab3e2
Akash <akash.patel9690@gmail.com>
2022-09-18 16:11:11 +0530

    Remaking

--- Task Bucket/index.js
+++ Task Bucket/index.js
@@ -1,38 +1,16 @@
-let taskList = document.querySelector(".task-list");
-// Inner HTML for new tasks
-let taskElements = '<div id="task-checkbox"></div> <input type="text" name="task-text" id="task-text">';
-let count = 1;
+let main = $(".main");
+let taskAdder = $(".add-task");
+let count = 0;
 
-// Added event listener to the first task
-let taskCheckbox1 = document.querySelector("#task-checkbox");
-taskCheckbox1.addEventListener("click", taskCheck);
-
-function addTask() {
+taskAdder.click(function() {
     count++;
-    // Adds new task
-    let indivTask = document.createElement("div");
-    indivTask.classList.add("indiv-task", "task"+count);
-    indivTask.innerHTML = taskElements;
-    taskList.appendChild(indivTask);
-    // Adds the class task(n) to task-checkbox
-    let checkbox = document.querySelector(".task"+count+" #task-checkbox");
-    checkbox.classList.add("task"+count);
-    document.querySelector(".task"+count+" #task-checkbox").style.backgroundImage='url("img/uncheck.png")';
-    // Adds an event listener to the new task created each time
-    let taskCheckbox = document.querySelectorAll("#task-checkbox");
-    taskCheckbox[count-1].addEventListener("click", taskCheck);
-}
+    let taskDiv = $("<div></div>");
+    taskDiv.addClass("tile task"+count);
+    taskDiv.html("<p class='heading'>Your Title Here</p> <p class='subtext'>Blah Blah Blah</p> <button class='delete-task'>-</button>");
+    main.prepend(taskDiv);
 
-function taskCheck() {
-    // Adds opacity=0.3 and disabled state to the respective textbox
-    let taskText = document.querySelector("."+this.className+' #task-text');
-    taskText.classList.toggle("done-task");
-    taskText.toggleAttribute("disabled");
-    // Toggles between Checked and Unchecked Image for the checkbox
-    let taskCheckboxImg = this.style.backgroundImage;
-    if(taskCheckboxImg=='url("img/uncheck.png")') {
-        this.style.backgroundImage='url("img/check.png")';
-    } else {
-        this.style.backgroundImage='url("img/uncheck.png")';
-    }
-}
+    $(".delete-task").click(function(event) {
+        event.remove();
+    })
+})
+
